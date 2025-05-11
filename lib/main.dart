dd858899fd19d36c3a99fc2c72dcfa3d6751dc3c
ydmins <dr.ydmins@gmail.com>
2025-05-12 07:57:40 +0900

    feature: add init_screen.dart and set routing of it in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+import 'screens/init_screen.dart';
+
+void main() {
+  runApp(App20250512());
+}
+
+class App20250512 extends StatelessWidget {
+  const App20250512({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App 20250512',
+      theme: ThemeData(primarySwatch: Colors.red),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InitScreen(),
+      },
+    );
+  }
+}
